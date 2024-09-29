6ff2eca9b5a8bf880d1af75a28c8433e63bd068f
Hassan <hassanahsan116@gmail.com>
2024-09-29 12:50:04 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using JokeApiConsumtion.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace JokeApiConsumtion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string url = "https://v2.jokeapi.dev/joke/Any";
+        HttpClient client = new HttpClient();
+       
+        public IActionResult Index()
+        {
+            Joke obj = new Joke();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var Data = JsonConvert.DeserializeObject<Joke>(result);
+                if (Data != null)
+                {
+                    ViewData["setup"] = Data.setup;
+                    ViewData["Delivery"] = Data.delivery;
+                }
+            }
+
+            return View();
+        }
+    }
+}
